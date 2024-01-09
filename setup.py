bd979ba785af1ca052b4b798fe39ef5597f026f1
princekumar2111 <pkyadavpr126@gmail.com>
2024-01-09 22:16:57 +0530

    Updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='Prince',
-    author_email='pkyadavpr126@gmail.com',
+    author='Group Project',
+    author_email='group_project@gmail.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
